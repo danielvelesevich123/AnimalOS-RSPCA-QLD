5f8da42be9bb316bf16c5ef891087755859a4d71
Danik Velesevich <danielvelesevich@gmail.com>
2025-09-30 09:56:11 +0300

    RQA-522

--- force-app/main/default/lwc/trapsMap/trapsMap.js
+++ force-app/main/default/lwc/trapsMap/trapsMap.js
@@ -1,4 +1,4 @@
-import {LightningElement} from 'lwc';
+import {LightningElement, api} from 'lwc';
 import {execute, showToast} from 'c/verticUtils';
 
 export default class TrapsMap extends LightningElement {
@@ -6,6 +6,17 @@ export default class TrapsMap extends LightningElement {
     mapMarkers = [];
     isBusy = false;
     selectedMarkerValue;
+    zoomLevel = 10;
+
+    defaultLocation = {
+        City: 'Brisbane',
+        Country: 'AU',
+        State: 'QLD'
+    };
+
+    mapCenter = {
+        location: this.defaultLocation
+    };
 
     connectedCallback() {
         this.refresh();
@@ -17,6 +28,8 @@ export default class TrapsMap extends LightningElement {
             let response = await execute('TrapsMapMetaProc', {});
             this.jobActivities = response.dto.jobActivities || [];
             this.initMapMarkers();
+            this.mapCenter.location = this.defaultLocation;
+            this.zoomLevel = 10;
         } catch (ex) {
             showToast(this, 'Error', Array.isArray(ex) ? ex[0].message : ex.message || ex.body.message, 'error');
         } finally {
@@ -49,5 +62,25 @@ export default class TrapsMap extends LightningElement {
 
     handleMarkerSelect(event) {
         this.selectedMarkerValue = event.target.selectedMarkerValue;
+    }
+
+    handleListItemClick(event) {
+        const id = event.currentTarget?.dataset?.id;
+        if (!id) { return; }
+        this.selectedMarkerValue = 'JOBACTIVITY' + id;
+        const found = this.mapMarkers.find(m => m.value === this.selectedMarkerValue);
+        if (found) {
+            this.mapCenter = { location: { ...found.location } };
+            this.zoomLevel = 13;
+        }
+    }
+
+    handleOpenRecordClick(event) {
+        event.stopPropagation();
+        const id = event.currentTarget?.dataset?.id;
+        if (id) {
+            const url = `${location.origin}/${id}`;
+            window.open(url, '_blank');
+        }
     }
 }
